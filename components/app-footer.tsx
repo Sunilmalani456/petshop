17f749ff580dc14c5e9a7b13425ae09b3f5df2d7
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-05-19 21:44:53 +0530

    add footer

--- components/app-footer.tsx
+++ components/app-footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const AppFooter = () => {
-  return <div>AppFooter</div>;
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5">
+      <small className="opacity-50">
+        &copy: 2030{" "}
+        <span className="font-extrabold underline">Sunilmalani456</span>. All
+        right reserved
+      </small>
+    </footer>
+  );
 };
 
 export default AppFooter;
