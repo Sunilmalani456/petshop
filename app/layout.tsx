f95627f3f427fea65e6859c334f1b8ffe99df20a
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-05-19 19:30:19 +0530

    setup complete also routing

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "PetSoft - Pet Daycare Software",
+  description:
+    "PetSoft is a pet daycare software for managing your pet daycare business. It helps you manage your pet daycare business with ease.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} min-h-screen text-sm text-zinc-900 bg-[#E5E8EC]`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
