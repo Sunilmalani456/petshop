bd67ac0a4ff3014f2d4f974364bc8e250c0483e3
Sunil Kumar Malani <sunilmalani456@gmail.com>
2024-05-24 00:22:25 +0530

    added pet details page

--- components/context/pet-context-provider.tsx
+++ components/context/pet-context-provider.tsx
@@ -12,6 +12,7 @@ type PetContextProps = {
   pets: pet[];
   selectedPetId: string | null;
   handleChangeSelectedPetId: (id: string) => void;
+  selectedPet: pet | undefined;
 };
 
 export const petContext = createContext<PetContextProps | null>(null);
@@ -20,13 +21,16 @@ const PetContextProvider = ({ data, children }: PetContextProviderProps) => {
   const [pets, setPets] = useState(data);
   const [selectedPetId, setSelectedPetId] = useState<string | null>(null);
 
+  // derived state
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+
   const handleChangeSelectedPetId = (id: string) => {
     setSelectedPetId(id);
   };
 
   return (
     <petContext.Provider
-      value={{ pets, selectedPetId, handleChangeSelectedPetId }}
+      value={{ pets, selectedPetId, selectedPet, handleChangeSelectedPetId }}
     >
       {children}
     </petContext.Provider>
